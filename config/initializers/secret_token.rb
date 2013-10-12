0ba97cdc0329a16f5805e34cf1f4fd7491ba7c86
Steve Gerety <sgerety@gmail.com>
2013-10-12 18:20:33 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pstest::Application.config.secret_key_base = '941b4eb9c08bd3ef5b20b067411fc9d46a81f5eef7adeb852836f375d043b8fdfe084366c240a852bcc28faa276efa77e98b64656b126c5f811c527190aca4bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pstest::Application.config.session_store :cookie_store, key: '_pstest_session'
